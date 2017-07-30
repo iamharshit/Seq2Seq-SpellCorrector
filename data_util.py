809f0b00539f9af120abfe6084c4f9a12378d88c
harshit@harshit <https://github.com/iamharshit>
2017-07-30 22:23:23 +0530

    first

--- data_util.py
+++ data_util.py
@@ -5,21 +5,50 @@ def judge(word):
     return word.isalpha()
 
 def clean(data):
-    try:
-        i=0
-        while(i<len(data)):
-            if judge(data[i][0])==False or judge(data[i][1])==False:
-                del data[i]
-                i-=1
-            i+=1
-    except:
-        pass
-    return data
+	#Remove Commas
+	try:
+		for i in range(len(data)):
+			if(type(data[i][0])==float  or type(data[i][1])==float):
+				del data[i]
+				continue
+			if(data[i][0][-1]==','):
+				data[i] = (data[i][0][:-1], data[i][1])
+			if(data[i][1][-1]==','):
+				data[i] = (data[i][0], data[i][1][:-1])
+	except:
+		pass	
+
+	try:
+		i=0
+		while(i<len(data)):
+			if judge(data[i][0])==False or judge(data[i][1])==False:
+				del data[i]
+				i-=1
+			i+=1
+	except:
+				pass
+	return data
+
+def remove_conflicting_examples(data):
+	correct_words, incorrect_words = np.array(data)[:,0], np.array(data)[:,1]
+	correct_vocab, incorrect_vocab = list(set(correct_words)), list(set(incorrect_words))
+   
+	try:
+		i=0
+		while(i<len(data)):
+			
+			if data[i][0] in incorrect_vocab or data[i][1] in correct_vocab:
+				del data[i]
+				i-=1
+			i+=1
+	except:
+		pass
+	return data	
 
 def load(filename):
     df = pd.read_csv(filename)
     ans = zip( list(df['Correct Words']), list(df['Incorrect Words']))
-    return clean(ans)
+    return remove_conflicting_examples(clean(ans ))
 
 def left_pad(list_):
     max_seq_len = 20
